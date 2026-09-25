c9c92f248b59f8521bf30de42494033f0926bf21
Harshita <harshitaworld9965@gmail.com>
2026-09-25 14:29:31 +0530

    onmouseenter

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const motion = useRef(null);
   const language = useRef(null);
   const letters = useRef([]);
+  const button = useRef(null);
+  const cards = useRef([]);
 
   useEffect(() => {
     const ctx = gsap.context(() => {
@@ -27,10 +29,15 @@ function App() {
       })
       gsap.from(letters.current, {
         y:-80,
-        duration:1.0,
+        duration:2.0,
         ease:"back.out",
-        stagger:0.12,
+        stagger:1,
       })
+      gsap.from(button.current, {
+  scale: 0,
+  duration: 1,
+  ease: "back.out",
+});
     });
 
     return ()=> ctx.revert();
@@ -50,6 +57,57 @@ function App() {
   <span ref={(el) => (letters.current[3] = el)}>I</span>
   <span ref={(el) => (letters.current[4 ] = el)}>O</span>
   <span ref={(el) => (letters.current[5] = el)}>N</span>
+</div>
+<button
+  ref={button}
+  className="magnetic-button"
+  onMouseEnter={() => {
+    gsap.to(button.current, {
+      scale: 1.1,
+      duration: 0.3,
+      ease: "power2.out",
+    });
+  }}
+  onMouseLeave={() => {
+    gsap.to(button.current, {
+      scale: 1,
+      duration: 0.3,
+      ease: "power2.out",
+    });
+  }}
+>
+  ENTER
+</button>
+<div className="cards">
+  <div ref={(el) => (cards.current[0]=el)} className="card">
+    <span>01</span>
+    <h2>MOTION</h2>
+  </div>
+
+  <div ref={(el) => (cards.current[1] = el)} className="card">
+    <span>02</span>
+    <h2>TYPE</h2>
+  </div>
+
+  <div ref={(el) => (cards.current[2] = el)} className="card"
+    onMouseEnter={() => {
+      gsap.to(cards.current[2], {
+        scale:1.2,
+        duration:0.3,
+        ease:"power2.out",
+      });
+    }}
+    onMouseLeave={() => {
+      gsap.to(cards.current[2], {
+        scale:1,
+        duration:0.5,
+        ease:"power2.out"
+      });
+    }}
+    >
+    <span>03</span>
+    <h2>FORM</h2>
+  </div>
 </div>
     </main>
   );
