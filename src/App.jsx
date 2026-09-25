202fb0e81705e328f425d00e4640038881c7d833
Harshita <harshitaworld9965@gmail.com>
2026-09-25 13:50:40 +0530

    stagger learning

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,55 @@ import gsap from "gsap";
 import './App.css'
 
 function App() {
+  const motion = useRef(null);
+  const language = useRef(null);
+  const letters = useRef([]);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.from(motion.current, {
+        x:-100,
+        duration:1,
+        ease:"power3.out"
+      })
+      gsap.from(language.current, {
+        y:100,
+        duration:1.5,
+        ease:"power2.out"
+      })
+      gsap.to(motion.current, {
+        x:50,
+        duration:0.5,
+        ease:"power2.inOut"        
+      })
+      gsap.from(letters.current, {
+        y:-80,
+        duration:1.0,
+        ease:"back.out",
+        stagger:0.12,
+      })
+    });
+
+    return ()=> ctx.revert();
+  }, []);
   return (
-    <main className="page">
-    <div className="card">
-      <span className="number">01</span>
-      <div className="card-content">
-        <p>EXPERIMENT</p>
-        <h1>MOTION</h1>
-        <h1>LAB</h1>
+    <main className="stage">
+      <p className="number">01</p>
+
+      <div className="type">
+        <h1 ref={motion}>MOTION</h1>
+        <h1 ref={language}>LANGUAGE.</h1>
       </div>
-    </div>
+      <div className="letters">
+  <span ref={(el) => (letters.current[0] = el)}>M</span>
+  <span ref={(el) => (letters.current[1] = el)}>O</span>
+  <span ref={(el) => (letters.current[2] = el)}>T</span>
+  <span ref={(el) => (letters.current[3] = el)}>I</span>
+  <span ref={(el) => (letters.current[4 ] = el)}>O</span>
+  <span ref={(el) => (letters.current[5] = el)}>N</span>
+</div>
     </main>
-  )
+  );
 }
+
 export default App;
